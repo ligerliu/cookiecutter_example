c402d6bce5953743d855dff9bc045406dff27365
Jiliang Liu <jiliangliu@bnl.gov>
2018-07-27 16:44:31 -0400

    add built html

---
+++ docs/build/html/_static/documentation_options.js
@@ -0,0 +1,9 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '0.post47.dev0+g7991fd2',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt'
+};
