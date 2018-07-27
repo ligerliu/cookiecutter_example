e25e507233d3338b4f69bd643532f8c7bb12a2a8
Travis CI User <travis@example.org>
2018-07-27 20:46:42 +0000

    Update docs after building Travis build 45 of ligerliu/package_example
    The docs were built from the branch 'master' against the commit
    c402d6bce5953743d855dff9bc045406dff27365.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/ligerliu/package_example/jobs/409110585.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.3/bin/doctr deploy --built-docs docs/build/html .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post48+g6c772b5',
+    VERSION: '0.post49+gc402d6b',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
